fcfcfc7780e848a1f359a8736cae1beb3dd9930f
heitinder <happymail72@gmail.com>
2014-12-16 15:44:26 -0800

    solved

--- HSTableViewController.h
+++ HSTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HSTableViewController.h
 //  UITableViewControllerApp
 //
 //  Created by Heitinder Singh on 12/16/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HSTableViewController : UITableViewController
 
 @end
-
